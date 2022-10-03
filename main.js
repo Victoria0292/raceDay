394f451272f26cf6882e5b37bdc22c1dad915725
Victoria0292 <tlynn0292@gmail.com>
2022-10-03 13:10:14 -0400

    else if statements added and comments main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Race Day project that will register runners and give instructions 
+//Race Day project that will give instructions to registered runners
 let raceNumber = Math.floor(Math.random() * 1000);
 const registeredEarly = true;
 const age = 30;
@@ -6,4 +6,15 @@ const age = 30;
 //checks if runner is an adult and registered early
 if (age > 18 && registeredEarly == true) {
   raceNumber += 1000;
+}
+
+//checks conditions of runner to see when they will race
+if (age > 18 && registeredEarly) {
+  console.log(`The race will begin at 09:30 am for runner: ${raceNumber}`);
+} else if (age > 18 && !registeredEarly) {
+  console.log(`The race will begin at 11:00 am for runner: ${raceNumber}`);
+} else if (age < 18) {
+  console.log(`The race will begin at 12:30 pm for runner: ${raceNumber}`);
+} else {
+  console.log("Please see registration desk.");
 }
